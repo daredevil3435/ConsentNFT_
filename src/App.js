df672bcf0056e79f00c88207ca214d45a742bbf1
miss_k <86659575+daredevil3435@users.noreply.github.com>
2023-07-20 00:00:45 +0530

    removed few warnings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import NFTForm from './components/NFTForm';
 import Demo from './components/Demo';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* <NFTForm /> */}
+      
       <Demo />
     </div>
   );
